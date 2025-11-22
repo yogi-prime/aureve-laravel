0e1c67bce27c24af4ac3f029116d249b7e4ac33c
yogi-prime <yogi@cre8r.in>
2025-11-22 09:42:43 +0530

    changes cors

--- config/cors.php
+++ config/cors.php
@@ -4,7 +4,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000', 'http://192.168.1.5:3000', 'https://www.aurevejewels.com/', 'https://app.aurevejewels.com/'], 'https://aurevejewels.com/',
+    'allowed_origins' => ['http://localhost:3000', 'http://192.168.1.5:3000', 'https://www.aurevejewels.com', 'https://app.aurevejewels.com'], 'https://aurevejewels.com',
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
